a96c9023702c2735ec9359c7843ac5b498a00a94
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2021-11-22 16:22:34 +0100

    chore: nouvel espace de nom pour C++

---
+++ cpp/include/View/ErrorView.hpp
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef ERRORVIEW_HPP
+#define ERRORVIEW_HPP
+
+class ErrorView
+{
+public: // pages
+  const void display_error() const;
+};
+
+#endif
--- cpp/src/View/HomeView.cpp
+++ cpp/src/View/HomeView.cpp
@@ -1,5 +1,8 @@
-#include "View/HomeView.hpp"
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#include "View/HomeView.hpp"
 #include "Encoder/RotaryEncoder.hpp"
 #include "Layout.hpp"
 #include "View.hpp"
